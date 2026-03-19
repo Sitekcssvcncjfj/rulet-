4c83050deb0e250271e6a3787c2d11e4592d823a
Sitekcssvcncjfj <98329599+Sitekcssvcncjfj@users.noreply.github.com>
2026-03-20 01:28:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+BOT_USERNAME = "KGBRUSRULETBOT"
+SUPPORT_URL = "https://t.me/KGBotomasyon"
+ADD_GROUP_URL = f"https://t.me/{BOT_USERNAME}?startgroup=true"
+
 DEFAULT_LOSS_CHANCE = 1 / 6
-DEFAULT_COOLDOWN = 15  # saniye
+DEFAULT_COOLDOWN = 15
